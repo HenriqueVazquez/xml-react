8053f9246904d8332dd58519d8311ede137002ff
Marcos Pereira <marcos.pereira@rcky.com.br>
2022-09-30 23:01:19 -0300

    feat: testando dados

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import XMLParser from "react-xml-parser";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState({});
+  const [count, setCount] = useState("");
 
   useEffect(() => {
     async function handleGetPriceList() {
@@ -17,8 +17,13 @@ function App() {
         const { data } = await axios.get("public/a.xml", config);
         console.log("data", data);
         const xmlToJson = new XMLParser().parseFromString(data);
-        console.log(xmlToJson);
-        setCount(xmlToJson);
+        console.log(
+          "nnfe",
+          xmlToJson.children[0].children[0].children[0].children[5].value
+        );
+        setCount(
+          xmlToJson.children[0].children[0].children[0].children[5].value
+        );
       } catch (error) {
         console.log("Error: ", error);
       }
@@ -27,7 +32,13 @@ function App() {
     handleGetPriceList();
   }, []);
 
-  return <div className="App">{count && <pre>{count.name}</pre>}</div>;
+  return (
+    <div className="App">
+      <ul>
+        <li>nNF: {count}</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
