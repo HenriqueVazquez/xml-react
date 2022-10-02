5e13176fe66da52bbd75bb98492a93372a653020
Marcos Pereira <marcos.pereira@rcky.com.br>
2022-10-02 14:06:06 -0300

    fix: ajustes gerais

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
+
 import XMLParser from "react-xml-parser";
 
 import "./App.css";
 
 function App() {
   const [xmlItems, setXmlItems] = useState<any>([]);
-  const [item, setItems] = useState<any>([]);
+  const [jsonXmlList, setJsonXmlList] = useState<any>([]);
 
   const handleFile = (event: any) => {
     const itensCopy = Array.from(event.target.files);
-    let final: any = [];
+    let processedFiles: any = [];
 
     itensCopy.forEach((item) => {
       let file = event.target.files[0];
@@ -18,19 +19,18 @@ function App() {
       reader.onloadend = () => {
         const xmlToJson = new XMLParser().parseFromString(reader.result);
 
-        let obj = {
+        let item = {
           nnf: xmlToJson.children[0]?.children[0].children[0].children[5].value,
           chave: xmlToJson.children[1]?.children[0].children[2].value,
           data: xmlToJson.children[0]?.children[0].children[0].children[6]
             .value,
         };
 
-        console.log("obg", obj);
-        setItems((prev: any) => [obj, ...prev]);
+        setJsonXmlList((prev: any) => [item, ...prev]);
       };
     });
 
-    setItems(final);
+    setJsonXmlList(processedFiles);
   };
 
   return (
@@ -40,12 +40,12 @@ function App() {
       <br />
       <br />
 
-      {item.map((nota: any, index: number) => {
+      {jsonXmlList.map((item: any, index: number) => {
         return (
           <tr key={index}>
-            <td>Nnf: {nota.nnf}</td>
-            <td>chave: {nota.chave}</td>
-            <td>data: {nota.data}</td>
+            <td>Nnf: {item.nnf}</td>
+            <td>chave: {item.chave}</td>
+            <td>data: {item.data}</td>
           </tr>
         );
       })}
