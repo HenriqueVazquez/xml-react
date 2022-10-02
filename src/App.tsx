681a96db5c3dc32276b35735177466e1427c226c
Marcos Pereira <marcos.pereira@rcky.com.br>
2022-10-02 13:59:22 -0300

    feat: consumindo json

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,54 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useState } from "react";
 import XMLParser from "react-xml-parser";
 
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState("");
-  const [key, setKey] = useState("");
-  const [creatAt, setCreatAt] = useState("");
-  const [total, setTotal] = useState("");
+  const [xmlItems, setXmlItems] = useState<any>([]);
+  const [item, setItems] = useState<any>([]);
 
-  useEffect(() => {
-    async function handleGetPriceList() {
-      const config = {
-        headers: { "Content-Type": "application/xml" },
+  const handleFile = (event: any) => {
+    const itensCopy = Array.from(event.target.files);
+    let final: any = [];
+
+    itensCopy.forEach((item) => {
+      let file = event.target.files[0];
+      let reader = new FileReader();
+      reader.readAsText(file, "windows-1251");
+      reader.onloadend = () => {
+        const xmlToJson = new XMLParser().parseFromString(reader.result);
+
+        let obj = {
+          nnf: xmlToJson.children[0]?.children[0].children[0].children[5].value,
+          chave: xmlToJson.children[1]?.children[0].children[2].value,
+          data: xmlToJson.children[0]?.children[0].children[0].children[6]
+            .value,
+        };
+
+        console.log("obg", obj);
+        setItems((prev: any) => [obj, ...prev]);
       };
+    });
 
-      try {
-        const { data } = await axios.get("public/a.xml", config);
-        const xmlToJson = new XMLParser().parseFromString(data);
-
-        setKey(xmlToJson.children[1].children[0].children[2].value);
-        setCount(
-          xmlToJson.children[0].children[0].children[0].children[5].value
-        );
-        setCreatAt(
-          xmlToJson.children[0].children[0].children[0].children[6].value
-        );
-        setTotal(
-          xmlToJson.children[0].children[0].children[11].children[0].children[1]
-            .value
-        );
-      } catch (error) {
-        console.log("Error: ", error);
-      }
-    }
-
-    handleGetPriceList();
-  }, []);
+    setItems(final);
+  };
 
   return (
     <div className="App">
-      <ul>
-        <li>nNF: {count}</li>
-        <li>Chave: {key}</li>
-        <li>Data: {creatAt}</li>
-        <li>Total: {total}</li>
-      </ul>
+      <input type="file" multiple accept="text/xml" onChange={handleFile} />
+
+      <br />
+      <br />
+
+      {item.map((nota: any, index: number) => {
+        return (
+          <tr key={index}>
+            <td>Nnf: {nota.nnf}</td>
+            <td>chave: {nota.chave}</td>
+            <td>data: {nota.data}</td>
+          </tr>
+        );
+      })}
     </div>
   );
 }
